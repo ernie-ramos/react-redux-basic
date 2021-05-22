7d40901246993e582afd982847fbc9ee9e26d71f
Ernesto Ramos <65619655+ernie-ramos@users.noreply.github.com>
2021-05-22 19:54:27 -0400

    destructured props in CommentDetail

--- components/src/CommentDetail.js
+++ components/src/CommentDetail.js
@@ -1,19 +1,19 @@
 import React from 'react';
 
-const CommentDetail = (props) => {
+const CommentDetail = ({ avatar, firstName, date, content }) => {
   return (
     <div className="comment">
       <a href="/" className="avatar">
-        <img alt="avatar" src={props.avatar} />
+        <img alt="avatar" src={avatar} />
       </a>
       <div className="content">
         <a href="/" className="author">
-          {props.firstName}
+          {firstName}
         </a>
         <div className="metadata">
-          <span className="date">{props.date}</span>
+          <span className="date">{date}</span>
         </div>
-        <div className="text">{props.content}</div>
+        <div className="text">{content}</div>
       </div>
     </div>
   );
