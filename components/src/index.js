ad5e629d69e214f6407669d447145b81a0cda3a4
Ernesto Ramos <65619655+ernie-ramos@users.noreply.github.com>
2021-05-22 19:57:29 -0400

    BRUH! destructured fakerAPI in index.js

--- components/src/index.js
+++ components/src/index.js
@@ -3,28 +3,28 @@ import ReactDOM from 'react-dom';
 
 import CommentDetail from './CommentDetail';
 
-const fakerAPI = require('./FakerAPI');
+const { avatar, firstName, date, content } = require('./FakerAPI');
 
 const App = () => {
   return (
     <div className="ui container comments">
       <CommentDetail
-        avatar={fakerAPI.avatar()}
-        firstName={fakerAPI.firstName()}
-        date={fakerAPI.date()}
-        content={fakerAPI.content()}
+        avatar={avatar()}
+        firstName={firstName()}
+        date={date()}
+        content={content()}
       />
       <CommentDetail
-        avatar={fakerAPI.avatar()}
-        firstName={fakerAPI.firstName()}
-        date={fakerAPI.date()}
-        content={fakerAPI.content()}
+        avatar={avatar()}
+        firstName={firstName()}
+        date={date()}
+        content={content()}
       />
       <CommentDetail
-        avatar={fakerAPI.avatar()}
-        firstName={fakerAPI.firstName()}
-        date={fakerAPI.date()}
-        content={fakerAPI.content()}
+        avatar={avatar()}
+        firstName={firstName()}
+        date={date()}
+        content={content()}
       />
     </div>
   );
