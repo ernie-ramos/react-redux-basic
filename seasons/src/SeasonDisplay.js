dbf9db18469ad84917e9c038efefcab344e851ec
Ernesto Ramos <65619655+ernie-ramos@users.noreply.github.com>
2021-05-23 15:31:08 -0400

    seasons app complete

--- seasons/src/SeasonDisplay.js
+++ seasons/src/SeasonDisplay.js
@@ -1,9 +1,35 @@
 import React from 'react';
 
-const SeasonDisplay = ({ lat, err }) => {
-  console.log(err);
+const seasonConfig = {
+  summer: {
+    text: "Let's hit the beach!",
+    iconName: 'sun',
+  },
+  winter: {
+    text: "Burr, it's cold!",
+    iconName: 'snowflake',
+  },
+};
 
-  return <div>{lat && !err ? `Latitude: ${lat}` : `Error: ${err}`}</div>;
+const getSeason = (lat, month) => {
+  if (month > 2 && month < 9) {
+    return lat > 0 ? 'summer' : 'winter';
+  } else {
+    return lat > 0 ? 'winter' : 'summer';
+  }
+};
+
+const SeasonDisplay = ({ lat }) => {
+  const season = getSeason(lat, new Date().getMonth());
+  const { iconName, text } = seasonConfig[season];
+
+  return (
+    <div>
+      <i className={`${iconName} icon huge`} />
+      <h1>{text}</h1>
+      <i className={`${iconName} icon huge`} />
+    </div>
+  );
 };
 
 export default SeasonDisplay;
--- seasons/src/index.js
+++ seasons/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
 
 class App extends React.Component {
   state = { lat: null, errorMessage: '' };
@@ -13,8 +14,19 @@ class App extends React.Component {
     );
   }
 
+  renderContent() {
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage}</div>;
+    }
+    if (!this.state.errorMessage && this.state.lat) {
+      return <SeasonDisplay lat={this.state.lat} />;
+    }
+
+    return <Spinner message="Please accept location request" />;
+  }
+
   render() {
-    return <SeasonDisplay lat={this.state.lat} err={this.state.errorMessage} />;
+    return this.renderContent();
   }
 }
 
