577667856e5c7031cfcd31e525ce326a7e52a268
Ernesto Ramos <65619655+ernie-ramos@users.noreply.github.com>
2021-05-22 20:37:11 -0400

    Created ApprovalCard for each CommentDetail

--- components/src/index.js
+++ components/src/index.js
@@ -2,30 +2,37 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
 const { avatar, firstName, date, content } = require('./FakerAPI');
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail
-        avatar={avatar()}
-        firstName={firstName()}
-        date={date()}
-        content={content()}
-      />
-      <CommentDetail
-        avatar={avatar()}
-        firstName={firstName()}
-        date={date()}
-        content={content()}
-      />
-      <CommentDetail
-        avatar={avatar()}
-        firstName={firstName()}
-        date={date()}
-        content={content()}
-      />
+      <ApprovalCard>
+        <CommentDetail
+          avatar={avatar()}
+          firstName={firstName()}
+          date={date()}
+          content={content()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          avatar={avatar()}
+          firstName={firstName()}
+          date={date()}
+          content={content()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          avatar={avatar()}
+          firstName={firstName()}
+          date={date()}
+          content={content()}
+        />
+      </ApprovalCard>
     </div>
   );
 };
