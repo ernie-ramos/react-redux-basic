c77bc0b26bfbb679d75c4291806149753a3f9457
Ernesto Ramos <65619655+ernie-ramos@users.noreply.github.com>
2021-05-23 14:29:59 -0400

    got rid of constructor()

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -4,18 +4,12 @@ import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
+  state = { lat: null, errorMessage: '' };
 
-    this.state = { lat: null, errorMessage: '' };
-
+  componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
-      (position) => {
-        this.setState({ lat: position.coords.latitude });
-      },
-      (err) => {
-        this.setState({ errorMessage: err.message });
-      }
+      (position) => this.setState({ lat: position.coords.latitude }),
+      (err) => this.setState({ errorMessage: err.message })
     );
   }
 
