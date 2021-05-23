ac44b0c177354cf97012eb6f33fbbf435c2bbe9f
Ernesto Ramos <65619655+ernie-ramos@users.noreply.github.com>
2021-05-23 18:58:55 -0400

    created searchBar

--- pics/src/index.js
+++ pics/src/index.js
@@ -1,10 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-class App extends React.Component {
-  render() {
-    return <div>Hello World</div>;
-  }
-}
+import App from './Components/App';
 
 ReactDOM.render(<App />, document.querySelector('#root'));
