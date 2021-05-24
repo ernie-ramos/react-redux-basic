6af0b410f4614ae1abb9b81829c6fce226040878
Ernesto Ramos <65619655+ernie-ramos@users.noreply.github.com>
2021-05-24 08:07:26 -0400

    added async api call to Unsplash

--- pics/src/Components/App.js
+++ pics/src/Components/App.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import axios from 'axios';
+
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
-  onSearchSubmit(term) {
-    console.log(term);
+  state = { images: [] };
+
+  async onSearchSubmit(term) {
+    const response = await axios.get('https://api.unsplash.com/search/photos', {
+      params: { query: term },
+      headers: {
+        Authorization: 'Client-ID Cc6UuT1dUBcJuzk1Is25bOiRgd0jarXmvcfKgA0JegA',
+      },
+    });
+
+    console.log(response.data.results);
   }
   render() {
     return (
