17e2142e8d36644bbf464c772e84f21551f5bfd3
Ernesto Ramos <65619655+ernie-ramos@users.noreply.github.com>
2021-05-26 10:53:30 -0400

    search widget complete

--- widgets/src/Components/Search.js
+++ widgets/src/Components/Search.js
@@ -18,8 +18,19 @@ const Search = () => {
       });
       setResults(data.query.search);
     };
-    if (term) {
+
+    if (term && !results.length) {
       search();
+    } else {
+      const timeoutId = setTimeout(() => {
+        if (term) {
+          search();
+        }
+      }, 500);
+
+      return () => {
+        clearTimeout(timeoutId);
+      };
     }
   }, [term]);
 
