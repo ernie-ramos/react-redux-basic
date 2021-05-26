ecf7926ac2a6a0183f8dc842d4547ae6a84d3a10
Ernesto Ramos <65619655+ernie-ramos@users.noreply.github.com>
2021-05-26 12:21:34 -0400

    dropdown widget complete

--- widgets/src/App.js
+++ widgets/src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
-import Search from './Components/Search';
+import React, { useState } from 'react';
+import Dropdown from './Components/Dropdown';
+// import Search from './Components/Search';
 // import Accordion from './Components/Accordion';
 
-const items = [
-  {
-    title: 'What is React?',
-    content: 'React is a front end javascript framework.',
-  },
-  {
-    title: 'Why use React?',
-    content: 'React is a favorite JS library among engineers.',
-  },
-  {
-    title: 'How do you use React?',
-    content: 'You use React by creating components.',
-  },
+// const items = [
+//   {
+//     title: 'What is React?',
+//     content: 'React is a front end javascript framework.',
+//   },
+//   {
+//     title: 'Why use React?',
+//     content: 'React is a favorite JS library among engineers.',
+//   },
+//   {
+//     title: 'How do you use React?',
+//     content: 'You use React by creating components.',
+//   },
+// ];
+
+const options = [
+  { label: 'The Color Red', value: 'red' },
+  { label: 'The Color Green', value: 'green' },
+  { label: 'A Shade of Blue', value: 'blue' },
 ];
 
 export default () => {
+  const [selected, setSelected] = useState(options[0]);
   return (
     <div>
-      <Search />
+      <Dropdown
+        selected={selected}
+        onSelectedChange={setSelected}
+        options={options}
+      />
     </div>
   );
 };
