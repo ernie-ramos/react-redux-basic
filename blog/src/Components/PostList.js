7dd0c920efdab30c803da8e461e9063dbd692ecb
Ernesto Ramos <65619655+ernie-ramos@users.noreply.github.com>
2021-06-24 16:09:41 -0400

    Update PostList.js

--- blog/src/Components/PostList.js
+++ blog/src/Components/PostList.js
@@ -5,7 +5,7 @@ import { fetchPosts } from '../actions';
 const PostList = ({ fetchPosts }) => {
   useEffect(() => {
     fetchPosts();
-  }, []);
+  }, [fetchPosts]);
 
   return <div className="ui container">Post List</div>;
 };
